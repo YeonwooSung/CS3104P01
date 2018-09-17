908091308651848bebdd83ce5dbb24830ffe85c3
YeonwooSung <neos960518@gmail.com>
2018-09-18 07:50:37 +0900

    trying to debug the return value of the printOut()

--- myls.c
+++ myls.c
@@ -1,8 +1,12 @@
+#include <stdio.h>
 #include "myls.h"
 
 // A complete list of linux system call numbers can be found in: /usr/include/asm/unistd_64.h
 #define WRITE_SYSCALL 1
 
 int main(int argc, char **argv) {
-    return printOut(argv[1]);
+    int i = printOut(argv[1]);
+    printf("\n%d\n", i);
+
+    return i;
 }
