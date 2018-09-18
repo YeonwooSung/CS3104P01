846415067074a74353e0e1e25bdae80b393163ee
YeonwooSung <neos960518@gmail.com>
2018-09-19 08:38:27 +0900

    add printf for debugging

--- myls.c
+++ myls.c
@@ -5,7 +5,9 @@
 int main(int argc, char **argv) {
     //check the number of the command line arguments
     if (argc == 2) {
-        printOut(argv[1]);
+        //printOut(argv[1]);
+        int i = openDirectory(argv[1]);
+        printf("%d", i);
     } else {
         char usageMsg[27] = "Usage: ./myls \"file_path\"\n";
         printOut(usageMsg);
