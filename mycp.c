abb50b5d1d4f55aee56fccaf4e3ce65b86666b45
YeonwooSung <neos960518@gmail.com>
2018-09-27 08:54:50 +0900

    start implementing the mycp (cp -r)

---
+++ mycp.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <time.h>
+#include <dirent.h>
+
+int main(int argc, char **argv) {
+    //trying to implement the recursive copy
+}
