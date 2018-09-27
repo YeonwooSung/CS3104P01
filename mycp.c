60d50c4b5c8079bb5f9ddc764e653d620ad3ac0b
YeonwooSung <neos960518@gmail.com>
2018-09-27 09:00:53 +0900

    add preprocessors for the system call numbers

--- mycp.c
+++ mycp.c
@@ -6,6 +6,15 @@
 #include <time.h>
 #include <dirent.h>
 
+/* system call numbers */
+#define READ_SYSCALL 0      //to read the file to copy the data of that file
+#define WRITE_SYSCALL 1     //to copy the data from the original file
+#define OPEN_SYSCALL 2      //to open the directory
+#define STAT_SYSCALL 4      //to get the file stat of the specific file.
+#define MMAP_SYSCALL 9      //to implement the custom malloc
+#define MUNMAP_SYSCALL 11   //to unmap the dynamically mapped memory
+#define GETDENTS_SYSCALL 78 //to get the directory entries
+
 int main(int argc, char **argv) {
     //trying to implement the recursive copy
 }
