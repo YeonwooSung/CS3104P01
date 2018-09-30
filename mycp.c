09b547739ff9f78dd608c17b3abb6fe02f43a354
YeonwooSung <neos960518@gmail.com>
2018-10-01 06:07:05 +0900

    now mycp could check if the source file exists

--- mycp.c
+++ mycp.c
@@ -16,6 +16,8 @@
 #define ACCESS_SYSCALL 21   //to check if the file exists
 #define GETDENTS_SYSCALL 78 //to get the directory entries
 
+#define OPEN_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
 /**
  * The custom strlen function.
  *
@@ -41,7 +43,7 @@ int strlength(const char *str) {
  * @param text the target text that should be printed out
  * @return ret If the syscall success, returns 1. Otherwise, returns -1.
  */
-int printOut(char *text) {
+int printOut(const char *text) {
     size_t len = strlength(text); //Length of our string, which we need to pass to write syscall
     long handle = 1;              //1 for stdout, 2 for stderr, file handle from open() for files
     long ret = -1;                //Return value received from the system call
@@ -54,6 +56,32 @@ int printOut(char *text) {
         "movq %%rax, %0\n\t" // %0 == ret
         : "=r"(ret)          /* if the syscall success, 1 will be stored in the ret. */
         : "r"((long)WRITE_SYSCALL), "r"(handle), "r"(text), "r"(len)
+        : "%rax", "%rdi", "%rsi", "%rdx", "memory");
+
+    return ret;
+}
+
+/**
+ * This function opens the file by using the syscall.
+ * The openDirectory() will be used to open the directory to read the files in it.
+ * This function uses the extended inline assembler to make interaction with the kernel more explicit.
+ *
+ * @param name the name of the directory that should be opened
+ * @return ret If the syscall success, the lowest numbered unused file descriptor will be returned. Otherwise, returns some negative value.
+ */
+int openDirectory(char *name) {
+    long ret = -1;
+    int flag = O_RDONLY;
+    int mode = OPEN_MODE;
+
+    asm("movq %1, %%rax\n\t" // %1 = (long) OPEN_SYSCALL
+        "movq %2, %%rdi\n\t" // %2 = name
+        "movq %3, %%rsi\n\t" // %3 = flag
+        "movq %4, %%rdx\n\t" // %4 = mode
+        "syscall\n\t"
+        "movq %%rax, %0\n\t"
+        : "=r"(ret)
+        : "r"((long)OPEN_SYSCALL), "r"(name), "r"((long)flag), "r"((long)mode) //convert the type from int to long for the movq instruction
         : "%rax", "%rdi", "%rsi", "%rdx", "memory");
 
     return ret;
@@ -95,10 +123,18 @@ void printFileNotExists(char *fileName) {
     printOut(errMessage2);
 }
 
+/* mycp is a program that copies the source to the destination recursively. */
 int main(int argc, char **argv) {
 
     if (argc == 2) {
-        //copy recursively
+        if (accessToFile(argv[1]) != 0) { //use the access syscall to check if the file exists
+            printOut("mycp: cannot stat");
+            printOut(argv[1]);
+            printOut(": Cannot find such file or directory\n");
+        } else {
+            //
+        }
+
     } else {
         char usageMsg[38] = "Usage: ./mycp \"SOURCE\" \"DESTINATION\"\n";
         printOut(usageMsg);
